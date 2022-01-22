da4116fa807453b86547dcb10ad317c3f67cbe3d
Harris Sultan <harris_sultan@outlook.com>
2022-01-22 17:32:59 -0500

    commit tree 9fcb7dee1693f67ecd5c53f2258a3305a1c10a93

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function(){
+    console.log("The DOM has Loaded");
+    document.getElementById('text').textContent='This is really cool!';
+});
+console.log("this loaded first")
